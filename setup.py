03b7b194fb97ddf2dd41cb76120bebeeb3732eb5
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-05-11 14:32:01 +0530

    Tox: Setup.py required.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="semesterstats",
+    version="0.1",
+    packages=find_packages("./"),
+    maintainer="Rushyanth111",
+    maintainer_email="Rushyanth111@gmail.com",
+    description="A Program to automate everything VTU"
+)
