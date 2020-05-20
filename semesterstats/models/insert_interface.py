5c79d236893bfec729f0ffd595a07ed901adad3e
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-05-20 10:18:37 +0530

    Semesterstats: Insert Interface Methods are completed.

--- semesterstats/models/insert_interface.py
+++ semesterstats/models/insert_interface.py
@@ -3,8 +3,15 @@
 # The rationale behind these interfaces is that any single parser later
 # on can be used.
 from typing import List
-from .basic_models import Department
-from .insert_interface_models import DepartmentModel
+from .basic_models import Department, Student, Subject, Score, Teacher, TeacherTaught
+from .insert_interface_models import (
+    DepartmentModel,
+    StudentModel,
+    SubjectModel,
+    TeacherModel,
+    ScoreModel,
+    TeacherTaughtModel,
+)
 from peewee import SqliteDatabase
 
 
@@ -15,35 +22,53 @@ class InsertInterface:
         pass
 
     def insert_department(self, department: DepartmentModel) -> bool:
-        record, created = Department.get_or_create(
-            DepartmentCode=department.DepartmentCode,
-            DepartmentName=department.DepartmentName,
-        )
-        return created
+        return Department.insert(department.__dict__).on_conflict_ignore().execute()
 
     def insert_department_bulk(self, department_list: List[DepartmentModel]) -> int:
-        department_tuples = []
-        for x in department_list:
-            department_tuples.append(x.__dict__)
+        dept_dicts = [x.__dict__ for x in department_list]
 
         with self.db.atomic():
-            lines_changed = Department.insert_many(department_tuples)
+            lines_changed = (
+                Department.insert_many(dept_dicts).on_conflict_ignore().execute()
+            )
         return lines_changed
 
-    def insert_student(self, student_record):
-        pass
+    def insert_student(self, student_record: StudentModel):
+        return Student.insert(student_record.__dict__).on_conflict_ignore().execute()
 
-    def insert_score(self, score_record):
-        pass
+    def insert_student_bulk(self, student_records: List[StudentModel]):
+        student_dicts = [x.__dict__ for x in student_records]
 
-    def insert_teacher(self, teacher_record):
-        pass
+        with self.db.atomic():
+            lines_changed = (
+                Student.insert_many(student_dicts).on_conflict_ignore().execute()
+            )
+        return lines_changed
 
-    def insert_student_bulk(self):
-        pass
+    def insert_score(self, score_record: ScoreModel):
+        return Score.insert(score_record.__dict__).on_conflict_ignore().execute()
 
-    def insert_score_bulk(self):
-        pass
+    def insert_score_bulk(self, score_records: List[ScoreModel]):
+        score_dicts = [x.__dict__ for x in score_records]
 
-    def insert_teacher_bulk(self):
+        with self.db.atomic():
+            lines_changed = Score.replace_many(score_dicts).execute()
+        return lines_changed
+
+    def insert_subject(self, subject_record: SubjectModel):
+        return Subject.insert(subject_record.__dict__).on_conflict_ignore().execute()
+
+    def insert_subjects(self, subject_records: List[SubjectModel]):
+        subject_dicts = [x.__dict__ for x in subject_records]
+
+        with self.db.atomic():
+            lines_changed = (
+                Subject.insert_many(subject_dicts).on_conflict_ignore().execute()
+            )
+        return lines_changed
+
+    def insert_teacher(self, teacher_record: TeacherModel):
+        return Teacher.insert(teacher_record.__dict__).on_conflict_ignore().execute()
+
+    def insert_teacher_bulk(self, teacher_records: List[TeacherModel]):
         pass
