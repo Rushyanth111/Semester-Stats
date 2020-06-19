4737bede85239d61d48e6b1f440a831afd4e4c4d
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-06-19 13:25:49 +0530

    Added ModelInterface as a singleton.

--- semesterstats/config.py
+++ semesterstats/config.py
@@ -1,6 +1,10 @@
 # This module initiates all of the required modules
 # and keeps them as a global access plane.
 import configparser
+from .models import ModelInterface
+
 
 config = configparser.ConfigParser()
 config.read("config.ini")
+
+db = ModelInterface(database_name="imported/data.db")
