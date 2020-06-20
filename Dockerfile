eb81027bfb5422ff450636a1a8fbcf159c73c3ee
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-06-20 20:13:07 +0530

    Dockerfile for deployments.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
-FROM python:3.6
+FROM python:3.6-slim
 
-COPY . ./dist
+COPY semesterstats ./semesterstats
+COPY FormattedData ./FormattedData
+COPY Resources ./Resources
+COPY requirements.txt ./
+
+RUN mkdir -p ./imported
+RUN pip install -r requirements.txt && rm -rf /var/lib/apt/lists/*
+
+CMD ["python", "-m", "semesterstats"]
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+import glob
 
 setup(
     name="semesterstats",
     version="0.1",
-    packages=find_packages("./"),
+    packages=find_packages(),
     maintainer="Rushyanth111",
     maintainer_email="Rushyanth111@gmail.com",
-    description="A Program to automate everything VTU"
+    description="A Program to automate everything VTU",
+    data_files=[
+        ("datafiles", glob.glob("FormattedData/*")),
+        ("resources", glob.glob("Resources/*")),
+    ],
+    zip_safe=False,
+    py_modules=["semesterstats"],
 )
