99d94646addffaf1fee49ab641c247c9609f339f
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-15 22:50:00 +0530

    Common::Extractor:feature//Added a function to check for diploma students.

--- semesterstat/common/extractor.py
+++ semesterstat/common/extractor.py
@@ -1,8 +1,7 @@
 import re
 
-
+# Format of the USN: (Uni Code{3})(Batch{2})(Dept{2})(RollNo{3})
 def batch_from_usn(usn: str) -> int:
-    # Format of the USN: (Uni Code{3})(Batch{2})(Dept{2})(RollNo{3})
     restr = "[A-Za-z0-9]{3}([0-9]{2})[A-Z]{2}[0-9]{3}"
 
     res = re.search(restr, usn).group(1)
@@ -15,26 +14,58 @@ def batch_from_usn(usn: str) -> int:
 
 
 def dept_from_usn(usn: str) -> str:
-    # Format of the USN: (Uni Code{3})(Batch{2})(Dept{2})(RollNo{3})
+
     restr = "[A-Za-z0-9]{3}[0-9]{2}([A-Z]{2})[0-9]{3}"
 
     res = re.search(restr, usn).group(1)
 
-    # Regex can be None as well, Incase of Misplaced Data.
     if res is None:
         return None
     else:
         return res
 
 
+def is_diploma(usn: str) -> bool:
+    restr = "[A-Za-z0-9]{3}[0-9]{2}[A-Z]{2}([0-9]{3})"
+
+    res = re.search(restr, usn).group(1)
+
+    if res is None:
+        return None
+    else:
+        return True if int(res) >= 400 else False
+
+
 # Format of SubjetCode: (Scheme)(Dept/SubjectCode)(Semester,SubjectNumber)
 def semester_from_subject(subcode: str) -> int:
-    pass
+    restr = "[0-9]{2}[A-Z]{2,6}([0-9]{2,3})"
+
+    res = re.search(restr, subcode).group(1)
+
+    if res is None:
+        return None
+    else:
+        return int(res)[0]
 
 
 def scheme_from_subject(subcode: str) -> int:
-    pass
+    restr = "([0-9]{2})[A-Z]{2,6}[0-9]{2,3}"
+
+    res = re.search(restr, subcode).group(1)
+
+    if res is None:
+        return None
+    else:
+        return 2000 + int(res)
 
 
 def dept_from_subject(subcode: str) -> str:
-    pass
+    restr = "[0-9]{2}([A-Z]{2,6})[0-9]{2,3}"
+
+    res = re.search(restr, subcode).group(1)
+
+    if res is None:
+        return None
+    else:
+        return res
+
