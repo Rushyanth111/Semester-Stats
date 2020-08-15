d4d6855665a7c6cd4f3f01d1386c0ffc6f2c3049
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-15 13:22:09 +0530

    Common::Extractor:Bug//Regex Group Capture was Missing

--- semesterstat/common/extractor.py
+++ semesterstat/common/extractor.py
@@ -3,22 +3,22 @@ import re
 
 def batch_from_usn(usn: str) -> int:
     # Format of the USN: (Uni Code{3})(Batch{2})(Dept{2})(RollNo{3})
-    restr = "[A-Za-z0-9]{3}([0-9]{3})[A-Z]{2}[0-9]{3}"
+    restr = "[A-Za-z0-9]{3}([0-9]{2})[A-Z]{2}[0-9]{3}"
 
-    res = re.search(restr, usn)
+    res = re.search(restr, usn).group(1)
 
     # Regex can be None as well, Incase of Misplaced Data.
     if res is None:
         return None
     else:
-        return int(res)
+        return 2000 + int(res)
 
 
 def dept_from_usn(usn: str) -> str:
     # Format of the USN: (Uni Code{3})(Batch{2})(Dept{2})(RollNo{3})
-    restr = "[A-Za-z0-9]{3}[0-9]{3}([A-Z]{2})[0-9]{3}"
+    restr = "[A-Za-z0-9]{3}[0-9]{2}([A-Z]{2})[0-9]{3}"
 
-    res = re.search(restr, usn)
+    res = re.search(restr, usn).group(1)
 
     # Regex can be None as well, Incase of Misplaced Data.
     if res is None:
@@ -27,6 +27,7 @@ def dept_from_usn(usn: str) -> str:
         return res
 
 
+# Format of SubjetCode: (Scheme)(Dept/SubjectCode)(Semester,SubjectNumber)
 def semester_from_subject(subcode: str) -> int:
     pass
 
