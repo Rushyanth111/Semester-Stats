16147ec4b66fc2499dba7e6e88d035ebca084c4d
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-17 17:04:35 +0530

    test::Api::Common::Report:test|| Use TestMaterial for USN Testing.

--- test/Api/Common/test_report.py
+++ test/Api/Common/test_report.py
@@ -1,25 +1,30 @@
 from semesterstat.api.common.reports import (
     StudentReport,
     SubjectReport,
-    ScoreReport,
-    Report,
 )
 import unittest
+import importlib_resources as pk
+from test import TestMaterial
+import csv
 
 
 class ReportTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        material = pk.read_text(TestMaterial, "Usn.csv").splitlines()
+        cls.materal = csv.reader(material, skipinitialspace=True)
+
     def test_student_report(self):
-        rep = StudentReport(Usn="1CX15CS152", Name="Some Random Name")
 
-        assert rep.Batch == 2015
-
-        assert rep.Department == "CS"
+        for usn, batch, dept, dip in self.materal:
+            batch = int(batch)
+            rep = StudentReport(Usn=usn, Name="Some Random Name")
+            self.assertEqual(rep.Batch, batch, "Batch is Not Correct")
+            self.assertEqual(rep.Department, dept, "Department is not Correct")
 
     def test_subject_report(self):
         rep = SubjectReport(Code="17MATDIP41", Name="Mathematics 4 DIP")
 
-        assert rep.Semester == 4
-
-        assert rep.Scheme == 2017
-
-        assert rep.Department == "XX"
+        self.assertEqual(rep.Semester, 4, "Semester is not Correct.")
+        self.assertEqual(rep.Scheme, 2017, "Scheme is not Correct")
+        self.assertEqual(rep.Department, "XX", "Department is not Correct")
