145499c9215e69acd78506182e8e9d9addaeb627
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 14:35:29 +0530

    Config: Updated to not rely on System Variables.

--- semesterstat/Config.py
+++ semesterstat/Config.py
@@ -1,18 +1,15 @@
 import configparser
-import os
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 is_dev = False
 
-if os.environ.get("APP_MODE") is None or os.environ.get("APP_MODE") == "DEV":
-    is_dev = True
-
 formatted_data_path = config["Routes"]["FilePath"]
 database_store_path = config["Routes"]["DataBasePath"]
+developer_app_mode = config["Developer"]["ApplicationMode"]
 # If the mode is developement, then change these variables.
 
-
-if is_dev:
+if developer_app_mode == "Dev":
     database_store_path = ":memory:"
+    is_dev = True
