0a0caecee56bd71d831e8cb0a879a3d95a0e1b4f
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 12:21:32 +0530

    Common::Query|| Peewee -> SQLAlchemy

--- semesterstat/common/query.py
+++ semesterstat/common/query.py
@@ -1,29 +1,26 @@
 from ..database import Subject, BatchSchemeInfo, Student
 from typing import List
+from sqlalchemy.orm import Session
 
 
-def get_subject_list(semester: int, scheme: int) -> List[str]:
+def get_subject_list(db: Session, semester: int, scheme: int) -> List[str]:
     # Returns the Subject List of a given Semester.
-    return [
-        sub
-        for sub in Subject.select(Subject.Code).where(
-            (Subject.Scheme == scheme) & (Subject.Semester == semester)
-        )
-    ]
-
-
-def get_scheme(batch: int) -> str:
     return (
-        BatchSchemeInfo.select(BatchSchemeInfo.Scheme)
-        .where((BatchSchemeInfo.Batch == batch))
-        .execute()
+        db.query(Subject.Code)
+        .filter(Subject.Semester == semester, Subject.Scheme == scheme)
+        .all()
     )
 
 
-def get_student_usn_list(batch: int, department: str) -> List[str]:
-    return [
-        student.Usn
-        for student in Student.select(Student.Usn).where(
-            (Student.Department == department) & (Student.Batch == batch)
-        )
-    ]
+def get_scheme(db: Session, batch: int) -> str:
+    return (
+        db.query(BatchSchemeInfo.Scheme).filter(BatchSchemeInfo.Batch == batch).first()
+    )
+
+
+def get_student_usn_list(db: Session, batch: int, department: str) -> List[str]:
+    return (
+        db.query(Student.Usn)
+        .filter(Student.Batch == batch, Student.Department == department)
+        .all()
+    )
