9c34429ab96b385fc4b5e045eb4023909e856b44
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 13:02:33 +0530

    Database::style|| Added typing hints to session_create()

--- semesterstat/database/database.py
+++ semesterstat/database/database.py
@@ -1,5 +1,7 @@
+from typing import Callable
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 from ..Config import database_store_path
 
 # Configuration of SQLAlchemy.
@@ -8,4 +10,6 @@ database_store_path = "sqlite:///{}".format(database_store_path)
 engine = create_engine(database_store_path, connect_args={"check_same_thread": False})
 
 # Exporting the Session_create Object
-session_create = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+session_create: Callable[[], Session] = sessionmaker(
+    bind=engine, autocommit=False, autoflush=False
+)
