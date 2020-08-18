54334de9753c00782878a9587aa6e477a91fd276
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 16:39:44 +0530

    Api::BatchQOL||Fixed Bad Imports.

--- semesterstat/api/batch/main.py
+++ semesterstat/api/batch/main.py
@@ -1,6 +1,8 @@
 from .results import results
+from .upload import upload
 from fastapi import APIRouter
 
 batch = APIRouter()
 
 batch.include_router(results, prefix="/result")
+batch.include_router(upload)
--- semesterstat/api/batch/results.py
+++ semesterstat/api/batch/results.py
@@ -1,35 +1,9 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-from ...database import Score
-from playhouse.shortcuts import model_to_dict
-from ...common.query import get_scheme, get_student_usn_list, get_subject_list
+from ..common.reports import ScoreReport
 
 results = APIRouter()
 
 
-class ScoreModel(BaseModel):
-    usn: str
-    subject_code: str
-    internals: int
-    externals: int
-
-
-@results.get("/{department}/{batch}/{semester}", response_model=ScoreModel)
-async def get_batch_results(department: str, batch: str, semester: int):
-    scheme = get_scheme(batch)
-
-    usn_list = get_student_usn_list(batch, department)
-
-    subject_code_list = get_subject_list(semester, scheme)
-
-    return [
-        model_to_dict(x, recurse=False)
-        for x in Score.select()
-        .where(
-            (
-                Score.ScoreSerialNumber.in_(usn_list)
-                & (Score.ScoreSubjectCode.in_(subject_code_list))
-            )
-        )
-        .objects()
-    ]
+@results.get("/{department}/{batch}/{semester}", response_model=ScoreReport)
+async def get_batch_results(department: str, batch: int, semester: int):
+    pass
