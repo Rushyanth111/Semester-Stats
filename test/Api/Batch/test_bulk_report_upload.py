75b78aaafc0d3591ceb87a0f2280f125d1891b09
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 16:30:58 +0530

    test::Api::Batch:test|| Added Conflicting Reporting.

--- test/Api/Batch/test_bulk_report_upload.py
+++ test/Api/Batch/test_bulk_report_upload.py
@@ -25,6 +25,30 @@ class BulkReportTest(unittest.TestCase):
 
         self.assertEqual(res.status_code, 201, "Creation of data did not happen")
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.client.close()
+    def test_upload_bulk_conflict(self):
+        data = [
+            {
+                "Usn": "1CR17CS115",
+                "Name": "Some Random Name",
+                "Subcode": "17CS51",
+                "Subname": "Enterprenuership and Something LOL",
+                "Internals": 5,
+                "Externals": 20,
+            },
+            {
+                "Usn": "1CR17CS115",
+                "Name": "Some Random Name",
+                "Subcode": "17CS51",
+                "Subname": "Enterprenuership and Something LOL",
+                "Internals": 5,
+                "Externals": 20,
+            },
+        ]
+
+        res = self.client.post("/batch/", json=[data[0]])
+
+        self.assertEqual(res.status_code, 201)
+
+        res = self.client.post("/batch/", json=[data[1]])
+
+        self.assertEqual(res.status_code, 201)
