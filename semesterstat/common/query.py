34743335d4c0ead24ae57ac8422276a8c2428efd
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 13:01:21 +0530

    Common::Query:Bug||Fixed Return of Tuples from SQLAlchemy ORM.

--- semesterstat/common/query.py
+++ semesterstat/common/query.py
@@ -5,22 +5,24 @@ from sqlalchemy.orm import Session
 
 def get_subject_list(db: Session, semester: int, scheme: int) -> List[str]:
     # Returns the Subject List of a given Semester.
-    return (
-        db.query(Subject.Code)
-        .filter(Subject.Semester == semester, Subject.Scheme == scheme)
-        .all()
-    )
+    return [
+        sub
+        for sub, in db.query(Subject.Code).filter(
+            Subject.Semester == semester, Subject.Scheme == scheme
+        )
+    ]
 
 
 def get_scheme(db: Session, batch: int) -> str:
     return (
         db.query(BatchSchemeInfo.Scheme).filter(BatchSchemeInfo.Batch == batch).first()
-    )
+    )[0]
 
 
 def get_student_usn_list(db: Session, batch: int, department: str) -> List[str]:
-    return (
-        db.query(Student.Usn)
-        .filter(Student.Batch == batch, Student.Department == department)
-        .all()
-    )
+    return [
+        student
+        for student, in db.query(Student.Usn).filter(
+            Student.Batch == batch, Student.Department == department
+        )
+    ]
