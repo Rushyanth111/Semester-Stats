a68099c51ceab1f1b4fd04d2d9890a1248b1130d
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 21:10:15 +0530

    Api::Dept:feat||Added Updating and Inserting For Department.

--- semesterstat/api/dept.py
+++ semesterstat/api/dept.py
@@ -1,17 +1,46 @@
 from fastapi import APIRouter, Response, status
 from fastapi.params import Depends
 from sqlalchemy.orm.session import Session
+from typing import Union
 
 from ..database import Department, get_db
-from ..common import DeparmentReceipt
+from ..common import DepartmentReceipt, DepartmentReport
 
 dept = APIRouter()
 
 
-@dept.get("/{department}", response_model=DeparmentReceipt)
+@dept.get("/{department}", response_model=DepartmentReceipt)
 def get_department(department: str, db: Session = Depends(get_db)):
     res = db.query(Department).filter(Department.Code == department).one_or_none()
     if res is None:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
-        return DeparmentReceipt(Code=res.Code, Name=res.Name)
+        return DepartmentReceipt(Code=res.Code, Name=res.Name)
+
+
+@dept.post("/")
+def add_department(
+    dept: DepartmentReport, resp: Response, db: Session = Depends(get_db)
+):
+    res = db.query(Department).filter(Department.Code == dept.Code).one_or_none()
+
+    if res is None:
+        obj = Department(Code=dept.Code, Name=dept.Name)
+        db.add(obj)
+    db.commit()
+
+
+@dept.put("/{department}", response_model=DepartmentReceipt)
+def update_department(
+    dept: DepartmentReport, resp: Response, db: Session = Depends(get_db)
+):
+    res: Union[Department, None] = db.query(Department).filter(
+        Department.Code == dept.Code
+    ).one_or_none()
+
+    if res is None:
+        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+    else:
+        res.Name = dept.Name
+
+    db.commit()
