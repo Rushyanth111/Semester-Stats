78ebb3340feeb5d7d89f7dce15b40f9dc48ab852
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-18 18:03:56 +0530

    database:Refactor || Changes to Pooling and Engine Classes.

--- semesterstat/database/database.py
+++ semesterstat/database/database.py
@@ -1,13 +1,21 @@
+from sqlalchemy.pool.impl import NullPool
 from typing import Callable
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.pool import StaticPool
 from ..Config import database_store_path
 
 # Configuration of SQLAlchemy.
-database_store_path = "sqlite:///{}".format(database_store_path)
 
-engine = create_engine(database_store_path, connect_args={"check_same_thread": False})
+DATABASE_PATH = "sqlite://{}".format(database_store_path)
+
+if database_store_path == "":
+    engine = create_engine(
+        DATABASE_PATH, connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+else:
+    engine = create_engine(DATABASE_PATH, poolclass=NullPool)
 
 # Exporting the Session_create Object
 session_create: Callable[[], Session] = sessionmaker(
