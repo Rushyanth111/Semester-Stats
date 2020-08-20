c2c768228b023f7984072ea47fd5ece4cfa5ad49
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-20 14:32:32 +0530

    tests::Common::Reports, Removed Reliance on testMaterial.

--- test/Common/test_report.py
+++ test/Common/test_report.py
@@ -3,24 +3,13 @@ from semesterstat.common import (
     SubjectReport,
 )
 import unittest
-import importlib_resources as pk
-from test import TestMaterial
-import csv
 
 
 class ReportTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        material = pk.read_text(TestMaterial, "Usn.csv").splitlines()
-        cls.materal = csv.reader(material, skipinitialspace=True)
-
     def test_student_report(self):
-
-        for usn, batch, dept, dip in self.materal:
-            batch = int(batch)
-            rep = StudentReport(Usn=usn, Name="Some Random Name")
-            self.assertEqual(rep.Batch, batch, "Batch is Not Correct")
-            self.assertEqual(rep.Department, dept, "Department is not Correct")
+        rep = StudentReport(Usn="1CR15CS401", Name="Some Random Name")
+        self.assertEqual(rep.Batch, 2015, "Batch is Not Correct")
+        self.assertEqual(rep.Department, "CS", "Department is not Correct")
 
     def test_subject_report(self):
         rep = SubjectReport(Code="17MATDIP41", Name="Mathematics 4 DIP")
