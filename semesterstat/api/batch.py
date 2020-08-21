5bb23b837e04fe7a7aa9e82c258a7be2ba8777be
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-22 02:56:07 +0530

    Api::Batch:Refactor|| Stylistic Changes.

--- semesterstat/api/batch.py
+++ semesterstat/api/batch.py
@@ -1,17 +1,11 @@
-from enum import Flag
+from typing import List, Optional, Union
 
-from fastapi.param_functions import Query
-from semesterstat.database.models import BatchSchemeInfo, Department
-from typing import List, Optional
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
 
-from fastapi import APIRouter, Depends, status
-from fastapi.params import Depends
-from sqlalchemy.orm import Session, session
-from starlette.responses import Response
-from uvicorn.config import logger
+from semesterstat.common.reports import ScoreReport
 
-from ..common import Report, ScoreReport
-from ..database import Score, Student, Subject, get_db
+from ..database import get_db
 
 batch = APIRouter()
 
@@ -24,33 +18,46 @@ batch = APIRouter()
 - Optional Filters.
     - detain: Bool = Only detained students returned.
     - listusn: Bool = Only USNs returned.
-    - detail: Bool = Returns the Full List of Details, Students, USN, Scores, etc.
     - backlogs: Bool = Returns the List of Backlogs attained by students in the filter.
 
 - Invalid Queries:
     - list and detail both True = Return a Bad Request.
 
+- Detail Endpoint:
+    - Takes in Mandatory FCD, FC, SC, Total, Avoid Subject
+    for Further Calculation.
+
+    - Expensive.
+
 """
 
 
-@batch.get("/{batch}/summary")
+@batch.get("/{batch}/summary", response_model=Union[List[ScoreReport], List[str]])
 async def get_batch_summary(
     batch: int,
     department: Optional[str] = None,
     semester: Optional[int] = None,
     detain: Optional[bool] = False,
     listusn: Optional[bool] = False,
-    detail: Optional[bool] = False,
     backlogs: Optional[bool] = False,
     db: Session = Depends(get_db),
 ):
-    if listusn is True and detail is True:
-        # Cannot be detailed and Summary at the same time.
-        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+    pass
 
-    # if (
-    #     db.query(BatchSchemeInfo).filter(BatchSchemeInfo.Batch == batch).one_or_none()
-    #     is None
-    # ):
-    #     return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+@batch.get("/{batch}/detail", response_model=Union[List[ScoreReport], List[str]])
+async def get_batch_detail(
+    batch: int,
+    fcd: int,
+    fc: int,
+    sc: int,
+    total: int,
+    avoid: List[str] = Query(...),
+    department: Optional[str] = None,
+    semester: Optional[int] = None,
+    detain: Optional[bool] = False,
+    listusn: Optional[bool] = False,
+    backlogs: Optional[bool] = False,
+    db: Session = Depends(get_db),
+):
     pass
