29a09318fe441aa8374a429b3330eb128e30bf69
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-23 23:32:17 +0530

    Crud::Dept:feat || Added Department Crud Operations.

--- semesterstat/crud/__init__.py
+++ semesterstat/crud/__init__.py
@@ -1,4 +1,20 @@
 from .batch import BatchQuery
 
+from .dept import (
+    get_dept_by_code,
+    get_dept_by_name,
+    get_dept_students,
+    get_dept_subjects,
+    is_dept_exist,
+    update_department,
+    put_department,
+)
 
-__all__ = ["BatchQuery"]
+__all__ = [
+    "BatchQuery",
+    "get_dept_by_code",
+    "get_dept_by_name",
+    "get_dept_students",
+    "get_dept_subjects",
+    "is_dept_exist",
+]
---
+++ semesterstat/crud/dept.py
@@ -0,0 +1,74 @@
+"""
+This Module Deals with Department Related Activities Only.
+
+get_dept -> Get Details of a Department.
+    - Filter Batch?
+"""
+
+from typing import List
+from sqlalchemy.orm import Session, noload
+
+from ..common import DepartmentReport, SubjectReport, StudentReport
+from ..database import Department, Subject, Student
+
+
+def get_dept_by_code(db: Session, code: str) -> DepartmentReport:
+    return DepartmentReport.from_orm(
+        db.query(Department)
+        .filter(Department.Code == code)
+        .options(noload(Department.Students), noload(Department.Subjects))
+        .first()
+    )
+
+
+def get_dept_by_name(db: Session, name: str) -> DepartmentReport:
+    return DepartmentReport.from_orm(
+        db.query(Department)
+        .filter(Department.Name.like(name))
+        .options(noload(Department.Students), noload(Department.Subjects))
+        .first()
+    )
+
+
+def get_dept_subjects(
+    db: Session, code: str, scheme: int = None
+) -> List[SubjectReport]:
+    res = db.query(Subject).filter(Subject.Department == code)
+
+    if scheme is not None:
+        res = res.filter(Subject.Scheme == scheme)
+
+    return [SubjectReport.from_orm(x) for x in res]
+
+
+def is_dept_exist(db: Session, code: str) -> bool:
+    res = db.query(Department).filter(Department.Code == code).one_or_none()
+
+    if res is not None:
+        return True
+
+    return False
+
+
+def get_dept_students(db: Session, code: str) -> List[StudentReport]:
+    res = db.query(Student).filter(Student.Department == code)
+
+    return [StudentReport.from_orm(x) for x in res]
+
+
+def put_department(db: Session, obj: DepartmentReport) -> None:
+    ins = Department(Code=obj.Code, Name=obj.Name)
+    db.add(ins)
+    db.flush()
+
+
+def update_department(
+    db: Session, old_obj: DepartmentReport, new_obj: DepartmentReport
+) -> bool:
+
+    upd = db.query(Department).filter(Department.Code == old_obj.Code).first()
+
+    upd.Code = new_obj.Code
+    upd.Name = new_obj.Name
+
+    db.flush()
