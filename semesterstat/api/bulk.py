56fa38d9a2267d326b15853c154b3d52243f6250
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-09-05 21:44:43 +0530

    Api::Bulk:Feat|| Added Bulk Methods for Respective Crud Methods.

--- semesterstat/api/bulk.py
+++ semesterstat/api/bulk.py
@@ -3,7 +3,13 @@ from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from ..common import Report
+from ..common import ScoreReport, DepartmentReport, StudentReport, SubjectReport
+from ..crud import (
+    put_department_bulk,
+    put_score_bulk,
+    put_student_bulk,
+    put_subject_bulk,
+)
 from ..database import get_db
 
 bulk = APIRouter()
@@ -15,6 +21,25 @@ and Adding new Reports.
 """
 
 
-@bulk.post("/", status_code=status.HTTP_201_CREATED, deprecated=True)
-async def upsert_bulk(reports: List[Report], db: Session = Depends(get_db)):
-    pass
+@bulk.post("/scores", status_code=status.HTTP_201_CREATED)
+async def score_bulk(reports: List[ScoreReport], db: Session = Depends(get_db)):
+    put_score_bulk(db, reports)
+    db.commit()
+
+
+@bulk.post("/dept", status_code=status.HTTP_201_CREATED)
+async def dept_bulk(reports: List[DepartmentReport], db: Session = Depends(get_db)):
+    put_department_bulk(db, reports)
+    db.commit()
+
+
+@bulk.post("/student", status_code=status.HTTP_201_CREATED)
+async def student_bulk(reports: List[StudentReport], db: Session = Depends(get_db)):
+    put_student_bulk(db, reports)
+    db.commit()
+
+
+@bulk.post("/subject", status_code=status.HTTP_201_CREATED)
+async def subject_bulk(reports: List[SubjectReport], db: Session = Depends(get_db)):
+    put_subject_bulk(db, reports)
+    db.commit()
