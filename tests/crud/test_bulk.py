a02a9179b5be09c75b90242262d599cfbb04e035
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-09-05 21:33:26 +0530

    tests::Crud::Bulk:test|| Added tests for All Functions.

--- tests/crud/test_bulk.py
+++ tests/crud/test_bulk.py
@@ -1,5 +1,19 @@
-from semesterstat.common.reports import ScoreReport
-from semesterstat.crud import put_score_bulk, get_student_subject
+from semesterstat.crud.subject import get_subject, is_subject_exist
+from semesterstat.crud.student import get_student, is_student_exists
+from semesterstat.crud.bulk import put_student_bulk, put_subject_bulk
+from semesterstat.common.reports import (
+    DepartmentReport,
+    ScoreReport,
+    StudentReport,
+    SubjectReport,
+)
+from semesterstat.crud import (
+    put_score_bulk,
+    get_student_subject,
+    put_department_bulk,
+    is_dept_exist,
+    get_dept_by_code,
+)
 from sqlalchemy.orm import Session
 
 
@@ -23,5 +37,65 @@ def test_put_score_bulk(db: Session):
     res = get_student_subject(db, "1CR15CS101", "15CS64")
     assert res.Internals == 30
     assert res.Externals == 50
+
+    db.rollback()
+
+
+def test_put_department_bulk(db: Session):
+    ins_list = [
+        DepartmentReport(Code=code, Name=name)
+        for (code, name) in [
+            ("XE", "Some Random Department"),
+            ("CS", "BadString"),  # This should be Ignored.
+        ]
+    ]
+
+    put_department_bulk(db, ins_list)
+    assert is_dept_exist(db, "XE")
+
+    res = get_dept_by_code(db, "CS")
+
+    assert res.Name == "Computer Science"
+
+    db.rollback()
+
+
+def test_put_student_bulk(db: Session):
+    ins_list = [
+        StudentReport(Usn=usn, Name=name)
+        for (usn, name) in [
+            ("1CR15CS001", "X"),
+            ("1CR15CS002", "X2"),
+            ("1CR15CS101", "X3"),
+        ]
+    ]
+
+    put_student_bulk(db, ins_list)
+    assert is_student_exists(db, "1CR15CS001")
+    assert is_student_exists(db, "1CR15CS002")
+
+    res = get_student(db, "1CR15CS101")
+    # No Change from Conftest
+    assert res.Name == "X"
+
+    db.rollback()
+
+
+def test_put_subject_bulk(db: Session):
+    ins_list = [
+        SubjectReport(Code=code, Name=name)
+        for (code, name) in [
+            ("15CS81", "X"),
+            ("15CS64", "X3"),
+        ]  # Last Shouldn't Shouldn't change
+    ]
+
+    put_subject_bulk(db, ins_list)
+
+    assert is_subject_exist(db, "15CS81")
+
+    res = get_subject(db, "15CS64")
+
+    assert res.Name == "X"
 
     db.rollback()
