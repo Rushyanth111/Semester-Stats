1173abaae175b83c48703cc62c6d0abd5ce6b90f
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-09-08 13:54:02 +0530

    Common::Generators:Feat|| Added Converters to Reports and Reciepts.

---
+++ semesterstat/common/generator.py
@@ -0,0 +1,28 @@
+from .reports import DepartmentReport, ScoreReport, StudentReport, SubjectReport
+from .reciepts import (
+    DepartmentReciept,
+    ScoreMinimalReciept,
+    ScoreReciept,
+    StudentReciept,
+    SubjectReciept,
+)
+
+
+def convert_dept(report: DepartmentReport) -> DepartmentReciept:
+    return DepartmentReciept.from_orm(report)
+
+
+def convert_student(report: StudentReport) -> StudentReciept:
+    return StudentReciept.from_orm(report)
+
+
+def convert_subject(report: SubjectReport) -> SubjectReciept:
+    return SubjectReciept.from_orm(report)
+
+
+def convert_score(report: ScoreReport) -> ScoreReciept:
+    return ScoreReciept.from_orm(report)
+
+
+def convert_score_minimal(report: ScoreReport) -> ScoreMinimalReciept:
+    return ScoreMinimalReciept.from_orm(report)
