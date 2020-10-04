d49434f10dc3892ea0b9925f7298c1b87c82043e
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-04 22:41:21 +0530

    Crud:Common: Added Doc strings.

--- semesterstat/crud/common.py
+++ semesterstat/crud/common.py
@@ -1,13 +1,21 @@
-from typing import Optional
-
 from sqlalchemy.orm import Session
 
 from ..database.models import BatchSchemeInfo
 
 
-def get_scheme(db: Session, batch: int) -> Optional[int]:
-    res = (
-        db.query(BatchSchemeInfo.Scheme).filter(BatchSchemeInfo.Batch == batch).scalar()
-    )
+def get_scheme(db: Session, batch: int) -> int:
+    """Obtain Scheme of Given Batch.
 
-    return res
+    Args:
+        db (Session): SQLAlchemy Database.
+        batch (int): Batch to be Found.
+
+    Raises:
+        NoResultFound
+
+    Returns:
+        int: The Scheme of the Given Batch.
+    """
+    res = db.query(BatchSchemeInfo.Scheme).filter(BatchSchemeInfo.Batch == batch).one()
+    final_res = res.Scheme
+    return final_res
--- tests/crud/test_common.py
+++ tests/crud/test_common.py
@@ -1,13 +1,24 @@
+from contextlib import nullcontext as does_not_raise
+
 import pytest
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from semesterstat.crud.common import get_scheme
 
 
 @pytest.mark.parametrize(
-    ["batch", "scheme"],
-    [(2015, 2015), (2016, 2015), (2017, 2017), (2014, None), (1, None), (None, None)],
+    ["batch", "scheme", "expectation"],
+    [
+        (2015, 2015, does_not_raise()),
+        (2016, 2015, does_not_raise()),
+        (2017, 2017, does_not_raise()),
+        (2014, None, pytest.raises(NoResultFound)),
+        (1, None, pytest.raises(NoResultFound)),
+        (None, None, pytest.raises(NoResultFound)),
+    ],
 )
-def test_get_scheme(db: Session, batch: int, scheme: int):
-    res = get_scheme(db, batch)
-    assert res == scheme
+def test_get_scheme(db: Session, batch: int, scheme: int, expectation):
+    with expectation:
+        res = get_scheme(db, batch)
+        assert res == scheme
