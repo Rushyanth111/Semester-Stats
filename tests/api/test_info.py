4249b80e2ead950fc577227cdd1e750864ffccaf
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-09 23:42:40 +0530

    tests:Api:Info:Delete|| Remove Old test_info

--- tests/api/test_info.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-
-
-@pytest.mark.parametrize(["batch", "op", "rescode"], [(2015, 2015, 200)])
-def test_info_route(client: TestClient, batch: int, op: int, rescode: int):
-    res = client.get("/info/scheme/{}".format(batch))
-    assert res.status_code == rescode
-    assert res.json() == op
