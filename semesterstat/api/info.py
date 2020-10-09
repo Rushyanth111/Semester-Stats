c5169b4ad26913aba0864fcabfb2f8f9ffdf3d98
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-09 23:44:24 +0530

    Api:Info:Feat|| Added Error Handling and Tests.

--- semesterstat/api/info.py
+++ semesterstat/api/info.py
@@ -3,8 +3,9 @@ Information Route, To get a lot of the Information Required.
 
 """
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from ..crud.common import get_scheme
 from ..database import get_db
@@ -12,6 +13,12 @@ from ..database import get_db
 info = APIRouter()
 
 
-@info.get("/scheme/{batch}", response_model=int)
+@info.get("/scheme/{batch}", response_model=int, status_code=status.HTTP_200_OK)
 def info_get_batch(batch: int, db: Session = Depends(get_db)):
-    return get_scheme(db, batch)
+    try:
+        res = get_scheme(db, batch)
+        return res
+    except NoResultFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Batch Doesn't Exist"
+        )
---
+++ tests/api/test_api_info.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+
+
+@pytest.mark.parametrize(
+    ["batch", "op", "rescode"],
+    [(2015, 2015, 200), (2014, {"detail": "Batch Doesn't Exist"}, 404)],
+)
+def test_info_route(client: TestClient, batch: int, op: int, rescode: int):
+    res = client.get("/info/scheme/{}".format(batch))
+    assert res.status_code == rescode
+    assert res.json() == op
