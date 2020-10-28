a9c29c5c5668c87d86ca29e0ba7930f0bc2e583a
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-28 19:47:43 +0530

    WebUI: Added Elements to Drawer.

--- semesterweb/src/Components/Drawer/Drawer.tsx
+++ semesterweb/src/Components/Drawer/Drawer.tsx
@@ -1,13 +1,19 @@
 import clsx from "clsx";
 import * as React from "react";
-import { ConnectedProps, connect } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
 
-import { createStyles, Toolbar, Typography } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
 import Drawer from "@material-ui/core/Drawer";
 import Fade from "@material-ui/core/Fade";
-import { makeStyles } from "@material-ui/core/styles";
-import AccountCircle from "@material-ui/icons/AccountCircle";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Subject from "@material-ui/icons/Book";
+import Home from "@material-ui/icons/Home";
+import Summary from "@material-ui/icons/Pages";
+import Batch from "@material-ui/icons/People";
+import Student from "@material-ui/icons/Person";
 
 import { RootState } from "../../Store";
 
@@ -23,24 +29,29 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      width: theme.spacing(10),
+      width: theme.spacing(7),
     },
     rootOpen: {
       width: theme.spacing(20),
     },
     rootPaper: {
-      width: theme.spacing(10),
+      width: theme.spacing(7),
       transition: theme.transitions.create(["all"], {
-        duration: 400,
+        duration: 600,
         delay: 0,
       }),
     },
     rootOpenPaper: {
       width: theme.spacing(20),
       transition: theme.transitions.create(["all"], {
-        duration: 400,
+        duration: 600,
         delay: 0,
       }),
+    },
+    buttonspacer: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
     },
   })
 );
@@ -64,14 +75,46 @@ function SemesterDrawer({ sideBarOpen }: PropsFromRedux): JSX.Element {
       }}
     >
       <Toolbar />
-      <Button>
-        <AccountCircle />
-        {sideBarOpen && (
-          <Fade in={sideBarOpen}>
-            <Typography variant="body1">Something Something</Typography>
-          </Fade>
-        )}
-      </Button>
+      <ListItem button>
+        <ListItemIcon>
+          <Home />
+        </ListItemIcon>
+        <Fade in={sideBarOpen} timeout={1200}>
+          <ListItemText primary="Home" />
+        </Fade>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <Batch />
+        </ListItemIcon>
+        <Fade in={sideBarOpen} timeout={1200}>
+          <ListItemText primary="Batch" />
+        </Fade>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <Student />
+        </ListItemIcon>
+        <Fade in={sideBarOpen} timeout={1200}>
+          <ListItemText primary="Student" />
+        </Fade>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <Subject />
+        </ListItemIcon>
+        <Fade in={sideBarOpen} timeout={1200}>
+          <ListItemText primary="Subject" />
+        </Fade>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <Summary />
+        </ListItemIcon>
+        <Fade in={sideBarOpen} timeout={1200}>
+          <ListItemText primary="Summary" />
+        </Fade>
+      </ListItem>
     </Drawer>
   );
 }
