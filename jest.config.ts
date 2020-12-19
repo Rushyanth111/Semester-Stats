79c4ea02b571f728958e4aa1da6b3b0e26eb5015
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-12-19 22:16:53 +0530

    WebUI:Jest|| TsModuleInter-Op Enable and Jest Fixes.

--- jest.config.ts
+++ jest.config.ts
@@ -146,12 +146,10 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/semesterweb/src/tests/*.tsx"],
+  testMatch: ["**/semesterweb/src/tests/**/*.(ts|tsx)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ["/node_modules/", "/setup/"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
