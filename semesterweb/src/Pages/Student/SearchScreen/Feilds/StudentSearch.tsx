d72d4f8777676cf8ef2a846c834b94b6d05398f9
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-12-19 22:58:45 +0530

    WebUI:StudentSearchScreen: Updated the Student search Screen.

--- semesterweb/src/Pages/Student/SearchScreen/Feilds/StudentSearch.tsx
+++ semesterweb/src/Pages/Student/SearchScreen/Feilds/StudentSearch.tsx
@@ -5,33 +5,66 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import { useHistory } from "react-router";
+import Snackbar from "@material-ui/core/Snackbar";
+import { isUsnValid } from "../../../../Helpers";
 
 const useStyles = makeStyles((theme: Theme) => ({
-  studentRootComponent: {
+  searchRoot: {
     display: "flex",
     flexDirection: "column",
     margin: theme.spacing(2),
     padding: theme.spacing(1),
   },
-  searchTypographyTitle: {
+  title: {
     alignSelf: "center",
     padding: theme.spacing(3),
   },
-  searchTextField: {
+  textFeild: {
     display: "flex",
     padding: theme.spacing(2),
   },
+  searchButtonContainer: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    padding: theme.spacing(3),
+    justifyContent: "space-evenly",
+  },
   searchButton: {
     alignSelf: "center",
+    padding: theme.spacing(1),
   },
 }));
 
 function StudentSearch(): JSX.Element {
   const classes = useStyles();
   const [usn, setUsn] = React.useState("");
+  const [isSnackbarOpen, setSnackbarOpen] = React.useState(false);
+  const [snackbarCount, setSnackbarCount] = React.useState(0);
   const history = useHistory();
-  const handleOnClickGoToStudent = () => {
-    history.push(`/Student/${usn}`);
+
+  const handleSnackbarClose = (): void => {
+    setSnackbarOpen(false);
+  };
+
+  const multipleSnackbarHandle = (): void => {
+    setSnackbarCount(snackbarCount + 1);
+    setSnackbarOpen(true);
+  };
+
+  const handleOnClickStudent = () => {
+    if (!isUsnValid(usn)) multipleSnackbarHandle();
+    else history.push(`/Student/${usn}`);
+  };
+
+  const handleOnClickStudentBacklogs = () => {
+    if (!isUsnValid(usn)) multipleSnackbarHandle();
+    else history.push(`/Student/Backlogs/${usn}`);
+  };
+
+  const handleOnClickStudentScores = () => {
+    if (!isUsnValid(usn)) multipleSnackbarHandle();
+    else history.push(`/Student/Scores/${usn}`);
   };
 
   const handleOnChangeTextField = (
@@ -41,11 +74,22 @@ function StudentSearch(): JSX.Element {
   };
 
   return (
-    <Paper className={classes.studentRootComponent} elevation={10}>
-      <Typography variant="h4" className={classes.searchTypographyTitle}>
+    <Paper className={classes.searchRoot} elevation={10}>
+      <Snackbar
+        open={isSnackbarOpen}
+        onClose={handleSnackbarClose}
+        autoHideDuration={3000}
+        message={"Hey, That's not a Valid USN."}
+        key={`StudentSearchPageSnackBar${snackbarCount}`}
+        anchorOrigin={{
+          horizontal: "center",
+          vertical: "top",
+        }}
+      />
+      <Typography variant="h4" className={classes.title}>
         Student Search
       </Typography>
-      <div className={classes.searchTextField}>
+      <div className={classes.textFeild}>
         <TextField
           id="something"
           variant="outlined"
@@ -56,14 +100,32 @@ function StudentSearch(): JSX.Element {
           onChange={handleOnChangeTextField}
         />
       </div>
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.searchButton}
-        onClick={handleOnClickGoToStudent}
-      >
-        Go!
-      </Button>
+      <div className={classes.searchButtonContainer}>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.searchButton}
+          onClick={handleOnClickStudent}
+        >
+          Go To Student Page
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.searchButton}
+          onClick={handleOnClickStudentBacklogs}
+        >
+          Go To Student Backlogs
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.searchButton}
+          onClick={handleOnClickStudentScores}
+        >
+          Go To Student Scores
+        </Button>
+      </div>
     </Paper>
   );
 }
